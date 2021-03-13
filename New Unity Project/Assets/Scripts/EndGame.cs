2ecfcaafbc2660c027c54702f9f5196dd07b7476
RomanJames <78179987+RomanJames@users.noreply.github.com>
2021-03-13 11:49:16 -0600

    aa game

---
+++ New Unity Project/Assets/Scripts/EndGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+
+{
+    public void NextLevel()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
